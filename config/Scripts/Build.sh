1ff6708f5692a422081438c928e938d099a8fbb7
Denislav Trifonov <denislavtrifonov@Denislavs-MacBook-Air.local>
2023-09-14 09:02:54 +0300

    Add ignore file

--- config/Scripts/Build.sh
+++ config/Scripts/Build.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
+# Shell script for building the STM32 based project using CMake and ninja.
+# It has to be executed in the main project dir
 
-# Script for build the project using CMake and ninja
+# Check if the build folder don't exist and create one
+if [ ! -d "build" ] 
+then
+  echo "Creating build folder!"
+  mkdir build
+else
+  echo "Build folder already exist"
+fi
+# Navigate to build folder
 cd build
+# Execute CMake
 cmake -GNinja -DCMAKE_TOOLCHAIN_FILE=../config/CMake/Toolchain.cmake -DCMAKE_BUILD_TYPE=Debug ../
+# Build the project using ninja build system
 ninja
+# Navigate back to project dir
 cd ../
